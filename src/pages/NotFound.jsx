b3cd025069f167f8c90ae0ab9f31b560c7eced8a
hong sipyo <155404215+SipYoHONG@users.noreply.github.com>
2024-04-11 12:00:21 +0900

    11일

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <div style={{margin: '20px'}}>
+            <h1>Page Not Found Error</h1>      
+            <img src='/img/404-page-not-found.svg' alt='error' />
+        </div>
+    )
+}
