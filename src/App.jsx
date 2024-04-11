52d4284e6cf8676a9a3fbb4e3c11a33d0ad11ac0
hong sipyo <155404215+SipYoHONG@users.noreply.github.com>
2024-04-11 12:54:00 +0900

    오전 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import SearchHeader from './components/SearchHeader';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Videos() {
+    const { keyword } = useParams();
+    const {isLoading, error, data:videos} = useQuery({
+        queryKey: ['videos', keyword], 
+        queryFn: async () => {
+            return axios
+                    .get(`/data/${keyword ? 'search' : 'popular'}.json`)
+                    .then(res => res.data.items);
+        },
+        staleTime: 1000 * 60 * 1,       // 1분, ms 단위로 지정할 수 있음
+    });
+    // useEffect(() => {
+    //     axios.get(`/data/${keyword ? 'search' : 'popular'}.json`)
+    //     .then(res => {
+    //         setVideos(res.data.items);
+    //         console.log(videos);
+    //     });
+    // }, [keyword]);
+
     return (
-        <div style={{margin: '20px'}}>
-            Videos Page
-        </div>
+        <>
+            <div> Videos {keyword ? `${keyword}로 검색` : 'Hot Trend'} </div>
+            <ul>
+                {videos.map(video => (
+                    <li key={video.id}>{video.snippet.title}</li>
+                ))}
+            </ul>
+        </>
     )
 }
