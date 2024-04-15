151412bafc3c73fd34229a70a46279189c538096
hong sipyo <155404215+SipYoHONG@users.noreply.github.com>
2024-04-15 09:58:35 +0900

    금요일 수정

---
+++ src/api/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export async function register(email, password) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+}
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
@@ -10,41 +11,51 @@ import Typography from '@mui/material/Typography';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 
 export default function SearchHeader() {
-    const { keyword } = useParams();
-    const navigate = useNavigate();
-    const [text, setText] = useState('');
+  const { keyword } = useParams();
+  const navigate = useNavigate();
+  const [text, setText] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(text);
+    navigate(`/videos/${text}`);
+  }
+  useEffect(() => {
+    setText(keyword || '');
+  }, [keyword]);
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        navigate(`/videos/${text}`);
-    }
-    useEffect(() => {
-        setText(keyword || '');
-    }, [keyword]);
-
-    return (
-        <Stack direction={'row'} sx={{alignItems: 'center'}}>
-            <Link to='/' style={{ textDecoration: 'none'}}>
-                <Stack direction={'row'} spacing={2}>
-                    <YouTubeIcon color='error' fontSize="large" />
-                    <Typography variant="h5" color='error' sx={{fontWeight: 'bold'}}>Youtube</Typography>
-                </Stack> 
+  return (
+    <header>
+      <Stack direction={'row'} sx={{alignItems: 'center'}}>
+        <Grid container>
+          <Grid item xs={3}>
+            <Link to='/' style={{textDecoration: 'none'}}>
+              <Stack direction={'row'} spacing={1}>
+                <YouTubeIcon color='error' fontSize="large" />
+                <Typography variant="h4" sx={{fontWeight: 'bold'}}>Youtube</Typography>
+              </Stack>
             </Link>
+          </Grid>
+          <Grid item xs={6}>
             <Paper
-                component="form" onClick={handleSubmit}
-                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, marginLeft:15 }}
-                >
-                <InputBase
-                    sx={{ ml: 1, flex: 1 }}
-                    placeholder="검색..."
-                    value={text}
-                    onChange={e => setText(e.target.value)}
-                />
-                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-                <IconButton color="primary" sx={{ p: '1' }} aria-label="directions">
-                    <SearchIcon />
-                </IconButton>
+              component="form" onSubmit={handleSubmit}
+              sx={{ p:'2px 4px', display:'flex', alignItems:'center', width:'100%' }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="검색..."
+                value={text} 
+                onChange={e => setText(e.target.value)}
+              />
+              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+              <IconButton type="button" sx={{ p: 1 }} aria-label="search" onClick={handleSubmit}>
+                <SearchIcon />
+              </IconButton>
             </Paper>
-        </Stack>
-    )
+          </Grid>
+          <Grid item xs={3}></Grid>
+        </Grid>
+      </Stack>
+      <Divider sx={{my: 1}} />
+    </header>
+  )
 }
